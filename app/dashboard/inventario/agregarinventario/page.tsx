1d63dacddbb7a290209fdb233b1f94b9ea363678
cyntitadecolores <cynthiasantt20@gmail.com>
2025-10-26 17:56:53 -0600

    page anterior para arreglar problema

--- app/dashboard/inventario/agregarinventario/page.tsx
+++ app/dashboard/inventario/agregarinventario/page.tsx
@@ -1,29 +1,31 @@
 'use client';
-import AddProductToStock from '@/components/AddProductToStock';
-import { useRouter, useSearchParams } from 'next/navigation';
 
+import { useRouter } from 'next/navigation';
+import { Suspense, useEffect, useState } from 'react';
+import AddProductToStock from '@/components/AddProductToStock';
 
 export default function AgregarProductoPage() {
   const router = useRouter();
-  const params = useSearchParams();
-  // lee el productId si viene en ?productId=123
-  const productIdParam = params.get('productId');
-  const initialProductId = productIdParam ? parseInt(productIdParam, 10) : undefined;
-  // lee optional locationId si viene en ?locationId=123
-  const locationIdParam = params.get('locationId');
-  const initialLocationId = locationIdParam ? parseInt(locationIdParam, 10) : undefined;
+  const [initialProductId, setInitialProductId] = useState<number | undefined>(undefined);
 
+  // Usamos useEffect para asegurarnos que useSearchParams solo se ejecute en el cliente
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const productIdParam = params.get('productId');
+    const productId = productIdParam ? parseInt(productIdParam, 10) : undefined;
+    setInitialProductId(productId);
+  }, []);
 
   return (
-    <div className='h-full m-5'>
-    <AddProductToStock
-      // le pasamos el id si lo hay, si no será undefined y el usuario lo elige manualmente
-      initialProductId={initialProductId}
-      initialLocationId={initialLocationId}
-      hideLocationSelect={Boolean(initialLocationId)}
-      onClose={() => router.push('/dashboard/inventario')}
-      onSaveStock={() => router.push('/dashboard/inventario')}
-    />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="h-full m-5">
+        <AddProductToStock
+          // le pasamos el id si lo hay, si no será undefined y el usuario lo elige manualmente
+          initialProductId={initialProductId}
+          onClose={() => router.push('/dashboard/inventario')}
+          onSaveStock={() => router.push('/dashboard/inventario')}
+        />
+      </div>
+    </Suspense>
   );
 }
